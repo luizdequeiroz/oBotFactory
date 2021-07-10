f1fa678ad5e5237ab0508abdda23902e2b79f977
Luiz de Queiroz <oluizdequeiroz@gmail.com>
2021-07-10 20:08:42 -0300

    configuring swagger for execute on all environments

--- api/Startup.cs
+++ api/Startup.cs
@@ -35,12 +35,12 @@ namespace api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
-            }
+            //if (env.IsDevelopment())
+            //{
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
+            //}
 
             app.UseHttpsRedirection();
 
